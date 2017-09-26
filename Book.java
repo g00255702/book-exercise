4e1438ac2544d16373f979400c28f61289ec3e23
Emeka Okonkwo <g00255702@stu.raritanval.edu>
2017-09-25 20:34:00 -0400

    Exercise 2.83

--- Book.java
+++ Book.java
@@ -11,18 +11,6 @@ class Book
     // The fields.
     private String author;
     private String title;
-    
-    //Exercise 2.85
-    private int pages;
-    
-    //Exercise 2.88
-    private String refNumber;
-    
-    //Exercise 2.91
-    private int borrowed;
-    
-    //Exercise 2.92
-    private boolean courseText;
 
     /**
      * Set the author and title fields when this object
@@ -32,22 +20,10 @@ class Book
     {
         author = bookAuthor;
         title = bookTitle;
-        
-        //Exercise 2.85
-        pages = bookPages;
-        
-        //Exercise 2.88
-        refNumber = "";
-        
-        //Exercise 2.92
-        courseText = bookCoursetext;
-        
-       
     }
 
     // Add the methods here ...
-    
-    //Exercise 2.83
+ 
     public String getAuthor()
     {
         return author;
@@ -56,100 +32,6 @@ class Book
     public String getTitle()
     {
         return title;
-    }
-    
-    //Exercise 2.84
-    public void printAuthor()
-    {
-        System.out.println(author);
-    }
-    
-    public void printTitle()
-    {
-        System.out.println(title);
-    }
-    
-    //Exercise 2.85
-    public int getPages()
-    {
-        return pages;
-    }
-    
-    //Exercise 2.87
-    /*
-    public void printDetails()
-    {
-        System.out.println("Title: " + title + ", " + "Author: " + author + ", " + "Pages: " + pages);
-    }
-    
-    
-    //Exercise 2.89
-    public void printDetails()
-    {
-        if(refNumber == "")
-        {
-            refNumber = "ZZZ";
-        }
-        else{
-        
-        System.out.println("Title: " + title + ", " + "Author: " + author + ", " + "Pages: " + pages + ", " + "Ref Number: " + refNumber);
-    }
-    }
-    */
-    //Exercise 2.91
-    public void printDetails()
-    {
-        if(refNumber == "")
-        {
-            refNumber = "ZZZ";
-        }
-        else{
-        
-        System.out.print("\n" + "Title: " + title + "\n" + "Author: " + author + "\n" + "Pages: " + pages + "\n" + "Ref Number: " + refNumber
-        + "\n" + "Books borrowed: " + borrowed + "\n");
-    }
-    }
-    //Exercise 2.88
-    /*
-    public void setRefNumber(String ref)
-    {
-        refNumber = ref;
-    }
-    */
-    
-    //Exercise 2.90
-    public void setRefNumber(String ref)
-    {
-        if(ref.length() < 3)
-        {
-            System.out.println("Invalid must be atleast 3 characters");
-        }
-        else
-        {
-            refNumber = ref;
-        }
-    }
-    
-    public String getRefNumber()
-    {
-        return refNumber;
-    }
-    
-    //Exercise 2.91
-    public void setBorrowed(int borrow)
-    {
-        borrowed += borrow;
-    }
-    
-    public int getBorrowed()
-    {
-        return borrowed;
-    }
-    
-    //Exercise 2.92
-    public boolean isCourseText()
-    {
-        return courseText;
     }
     
 }
